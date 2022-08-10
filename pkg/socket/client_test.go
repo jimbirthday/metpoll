b4ae087a41c20f73f2b00c2eb6fdea1ad3b8130b
jim <303600370@qq.com>
2022-08-11 00:42:14 +0800

    1

--- pkg/socket/client_test.go
+++ pkg/socket/client_test.go
@@ -1,7 +1,38 @@
 package socket
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"testing"
+)
 
 func TestClinet(t *testing.T) {
-
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("err : ", err)
+		return
+	}
+	defer conn.Close() // 关闭TCP连接
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := inputReader.ReadString('\n') // 读取用户输入
+		inputInfo := strings.Trim(input, "\r\n")
+		if strings.ToUpper(inputInfo) == "Q" { // 如果输入q就退出
+			return
+		}
+		_, err := conn.Write([]byte(inputInfo)) // 发送数据
+		if err != nil {
+			return
+		}
+		buf := [512]byte{}
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("recv failed, err:", err)
+			return
+		}
+		fmt.Println(string(buf[:n]))
+	}
 }
--- pkg/socket/server_test.go
+++ pkg/socket/server_test.go
@@ -2,13 +2,92 @@ package socket
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
+
+	"golang.org/x/sys/unix"
 )
 
 func TestServer(t *testing.T) {
 	_, _, err := TCPSocket("127.0.0.1:8080", true)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("TCPSocket:" + err.Error())
 		return
 	}
+	fd, err := unix.EpollCreate1(unix.EPOLL_CLOEXEC)
+	if err != nil {
+		fmt.Println("EpollCreate1:" + err.Error())
+		return
+	}
+	//没有设置 EFD_NONBLOCK 就是阻塞得，设置的话就是非租塞得返回-1
+	efd, err := unix.Eventfd(0, unix.EFD_NONBLOCK|unix.EPOLL_CLOEXEC)
+	if err != nil {
+		fmt.Println("EpollCreate1:" + err.Error())
+		return
+	}
+
+	unix.EpollCtl(fd, unix.EPOLL_CTL_ADD, efd, &unix.EpollEvent{Fd: int32(efd), Events: unix.EPOLLOUT})
+
+	go func() {
+		msec := -1
+		ls := make([]unix.EpollEvent, 128)
+		for {
+			n, err := unix.EpollWait(fd, ls, msec)
+			if n == 0 || (n < 0 && err == unix.EINTR) {
+				msec = -1
+				runtime.Gosched()
+				continue
+			} else if err != nil {
+				fmt.Println("EpollWait:" + err.Error())
+				return
+			}
+
+			msec = 0
+
+			for i := 0; i < n; i++ {
+				ev := &ls[i]
+				if fd := int(ev.Fd); fd != efd {
+					fmt.Println("EpollWait efd")
+				} else { // poller is awakened to run tasks in queues.
+					efdBuf := make([]byte, 8)
+					_, _ = unix.Read(efd, efdBuf)
+					fmt.Print(string(efdBuf))
+				}
+			}
+		}
+	}()
+	time.Sleep(time.Minute * 60)
 }
+
+// func TestTCP(t *testing.T) {
+// 	listen, err := net.Listen("tcp", "127.0.0.1:8080")
+// 	if err != nil {
+// 		fmt.Println("Listen() failed, err: ", err)
+// 		return
+// 	}
+// 	for {
+// 		conn, err := listen.Accept() // 监听客户端的连接请求
+// 		if err != nil {
+// 			fmt.Println("Accept() failed, err: ", err)
+// 			continue
+// 		}
+// 		go process(conn) // 启动一个goroutine来处理客户端的连接请求
+// 	}
+// }
+
+// func process(conn net.Conn) {
+// 	defer conn.Close() // 关闭连接
+// 	for {
+// 		reader := bufio.NewReader(conn)
+// 		var buf [128]byte
+// 		n, err := reader.Read(buf[:]) // 读取数据
+// 		if err != nil {
+// 			fmt.Println("read from client failed, err: ", err)
+// 			break
+// 		}
+// 		recvStr := string(buf[:n])
+// 		fmt.Println("收到Client端发来的数据：", recvStr)
+// 		conn.Write([]byte(recvStr)) // 发送数据
+// 	}
+// }
